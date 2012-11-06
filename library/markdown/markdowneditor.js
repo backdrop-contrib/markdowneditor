e4a5b0e7ea6da8120c5b34a62150ec8b0aa47f16
Fredrik Jonsson <frjo@xdeb.org>
2012-11-06 20:58:15 +0100

    Issue #1392218 by dhalbert: Image link dialog should allow inline URL.

--- library/markdown/markdowneditor.js
+++ library/markdown/markdowneditor.js
@@ -1555,13 +1555,15 @@ markdownEditor.image = function () {
   var referenceValue = "";
   var titleValue = "";
   var textValue = BUE.active.getSelection();
+  var inlineValue = null;
 
   // Creating the form for the dialog.
   var form = createForm(
-    { label : t("Short Desc."), mandatory : true, attributes : { name : "text", value : textValue } },
-    { label : t("Long Desc."), attributes : { name : "title", value : titleValue } },
+    { label : t("Alt"), mandatory : true, attributes : { name : "alt", value : textValue } },
+    { label : t("Title"), attributes : { name : "title", value : titleValue } },
     { label : t("Reference"), attributes : { name : "reference", value : referenceValue } },
-    { label : t("URL"), mandatory : true, attributes : { name : "href", value : hrefValue } }
+    { label : t("URL"), mandatory : true, attributes : { name : "href", value : hrefValue } },
+    { label : t("Inline"), attributes : { name : "inline", type : "checkbox", value: inlineValue } }
   );
 
   // Create an onsubmit handler and various buttons.
@@ -1588,16 +1590,17 @@ markdownEditor.image._process = function (form) {
   var t = markdownEditor.t;
 
   var referenceType = "Images";
-  var text = form.elements.text.value;
-  var reference = form.elements.reference.value || text;
-  var href = form.elements.href.value;
+  var alt = form.elements.alt.value;
   var title = form.elements.title.value;
+  var reference = form.elements.reference.value || alt;
+  var href = form.elements.href.value;
+  var inline = form.elements.inline.checked || false;
 
   // Validate input.
   markdownEditor.dialog.clearErrors();
   var valid = true;
-  if (!text) {
-    markdownEditor.dialog.addError(t("Text is a required field."));
+  if (!alt) {
+    markdownEditor.dialog.addError(t("Alt is a required field."));
     valid = false;
   }
   if (!href) {
@@ -1608,12 +1611,19 @@ markdownEditor.image._process = function (form) {
     return;
   }
 
-  // The text added at the caret position.
-  var textString = text ? "![" + text + "][" + reference + "]" : "![" + reference + "]";
-  // The reference to add to the reference section.
-  var ref = new Reference(referenceType, reference, href + (title ? ' "' + title + '"' : ""));
-
-  markdownEditor.references._callback(textString, ref);
+  if (inline) {
+    // Insert inline link after caret position
+    var replaceString = "![" + alt + "](" + href + ( title ? ' "' + title + '"' : '' ) + ")";
+    markdownEditor.selection.replaceAll(replaceString);
+    BUE.dialog.close();
+  }
+  else {
+    // The text added at the caret position.
+    var textString = alt ? "![" + alt + "][" + reference + "]" : "![" + reference + "]";
+    // The reference to add to the reference section.
+    var ref = new Reference(referenceType, reference, href + (title ? ' "' + title + '"' : ""));
+    markdownEditor.references._callback(textString, ref);
+  }
 };
 
 
